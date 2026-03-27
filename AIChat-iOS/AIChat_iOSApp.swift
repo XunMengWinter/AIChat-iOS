6091f7f4130a135057850a70133c4da38a505081
ice <XunMengWinter@gmail.com>
2026-03-27 14:21:46 +0800

    Initial Commit

---
+++ AIChat-iOS/AIChat_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChat_iOSApp.swift
+//  AIChat-iOS
+//
+//  Created by ice on 27/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AIChat_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChat-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIChat-iOS
+//
+//  Created by ice on 27/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
